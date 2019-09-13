e5cec3d35c34b3d104fee99761d03b8f2bb33159
XDavidT <XDavidT@gmail.com>
2019-09-13 15:47:55 +0300

    CHange variable name

--- MainService.py
+++ MainService.py
@@ -5,7 +5,6 @@ last_time_detla = datetime.datetime.now() - datetime.timedelta(hours = 2)
 client = MongoClient('mongodb://13.68.170.154:27017/')
 
 logs_collection = client['clientManager']['clientLog'] # Select DB
-print(start)
 
 with open('test.txt','a') as output:
     for log in logs_collection.find({'insert_time' : {'$gte' : last_time_detla} }):
