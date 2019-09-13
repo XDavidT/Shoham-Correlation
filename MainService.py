6df63e0cbad5084e7f9cb83b503063dfca5ccda7
XDavidT <XDavidT@gmail.com>
2019-09-13 15:47:45 +0300

    CHange variable name

--- MainService.py
+++ MainService.py
@@ -1,13 +1,12 @@
 import datetime
 from pymongo import MongoClient
 
-start = datetime.datetime.now() - datetime.timedelta(hours = 2)
-end = datetime.datetime.now()
+last_time_detla = datetime.datetime.now() - datetime.timedelta(hours = 2)
 client = MongoClient('mongodb://13.68.170.154:27017/')
 
 logs_collection = client['clientManager']['clientLog'] # Select DB
 print(start)
 
 with open('test.txt','a') as output:
-    for log in logs_collection.find({'insert_time' : {'$gte' : start} }):
+    for log in logs_collection.find({'insert_time' : {'$gte' : last_time_detla} }):
         output.write(str(log))
