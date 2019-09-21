097e7375c2c4a95cc227dc8ff35cb81c4617dd8b
David Tayar <xdavidt@gmail.com>
2019-09-21 23:36:16 +0300

    Manual Search + function to semi-alert

--- LogsTracker.py
+++ LogsTracker.py
@@ -20,24 +20,45 @@ class LogsTracker_service():
         logs_collection = client[setting['logs-db-name']][setting['logs-collection-name']]
         last_time_delta = datetime.datetime.now() - datetime.timedelta(hours=setting['logs-from-X-hours'])
 
+        logs = logs_collection.find({'insert_time': {'$gte': last_time_delta}}) # Logs from X last time
+        for log in logs:
+            for event in events:
+                rule = rules[event['rules'][0]['rule_id']]
 
-        for event in events:                            # Search for each event
-            rule = rules[event['rules'][0]['rule_id']]
+                if(log[rule['field']] == rule['value']):
+                    self.DumpDocumentToMongo(event['_id'],log)
+                    print('Found match !') # Dev print
+        # for event in events:                            # Search for each event
+        #     rule = rules[event['rules'][0]['rule_id']]
+        #     print(rule['field']+ ' <-> '+rule['value'])
+        #
+        #     # results = logs_collection.find(             # Build the query
+        #     #         {'insert_time': {'$gte': last_time_delta}},
+        #     #         {rule['field']:rule['value']})
+        #
+        #     for log in results:  # List is already from old time to last
+        #         print(log) # TODO: check the results
 
-            results = logs_collection.find(             # Build the query
-                    {'insert_time': {'$gte': last_time_delta}},
-                    {rule['field']:rule['value']})
-
-            for log in results:  # List is already from old time to last
-                print(log) # TODO: check the results
-
-        print("thread " + str(th.get_ident()) + " finish")
+        print("thread " + str(th.get_ident()) + " finish") # Dev print
 
     def LogsThreadStatus(self):
         return self.status
 
+    def DumpDocumentToMongo(self,event_id,log):
+        setting = Load_Setting()
+        client = Mongo_Connection()
+        log_dump = {
+            'event':event_id,
+            'step':1,
+            'log':[log]
+        }
 
-
+        try:
+            semi_open = client[setting['policy-db-name']]['semi-open']
+            semi_open.insert_one(log_dump)
+            print('Insert LOG OK.') # Dev print
+        except Exception as e:
+            print ("error ->"+str(e)) # Dev print
 
 
 
