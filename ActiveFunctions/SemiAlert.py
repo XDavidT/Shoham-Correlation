1407a5252a119361598ef53befb83bb1af5ad5fc
David Tayar <xdavidt@gmail.com>
2019-10-02 19:39:57 +0300

    Findlog - split to function

--- ActiveFunctions/SemiAlert.py
+++ ActiveFunctions/SemiAlert.py
@@ -26,22 +26,11 @@ def CheckSemi():
         time_delta = last_log_time + datetime.timedelta(seconds=timeout)  # TODO: check time interval before action
         logs_collection = client[setting['logs-db-name']][setting['logs-collection-name']]
 
-        if log_document['type'] == 'local':
-            first_log = logs_collection.find_one({
-                rule['field']: rule['value'],                       # Next rule
-                setting['local_based_on']:log_document['device'],   # Same device
-                "$lte":time_delta,                                  # Before: (Last log + Timeout) = delta
-                "$get":last_log_time})                              # After last log we have
-            #TODO: check by device
-            print(first_log)
-        else:
-            results = logs_collection.find({
-                rule['field']: rule['value'],                       # Next rule
-                "$lte":time_delta,                                  # Before: (Last log + Timeout) = delta
-                "$get":last_log_time})                              # After last log we have
-            for log in results:
-                print(log)
-        CheckRelevant(client,log_document,time_delta,setting)
+        log = FindLog(logs_collection,log_document,rule,setting,time_delta,last_log_time)
+        if (len(log) == 0): # If nothing returned
+            CheckRelevant(client,log_document,time_delta,setting)
+
+        # TODO: but if something was returned - do->
     print("Flag 2# is done !")
             # step = log_document['step']+1
             # rule = rules[log_document['rules'][step]['rule_id']] # Return current rule (+1 from back one)
@@ -62,8 +51,28 @@ def CheckSemi():
 
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
 
+def FindLog(logs_collection,log_document, rule,setting,time_delta,last_log_time):
+    if log_document['type'] == 'local':
+        return logs_collection.find_one({
+            rule['field']: rule['value'],  # Next rule
+            setting['local_based_on']: log_document['device'],  # Same device
+            "$lte": time_delta,  # Before: (Last log + Timeout) = delta
+            "$get": last_log_time})  # After last log we have
+    else:
+        log_list = []
+        results = logs_collection.find({
+            rule['field']: rule['value'],  # Next rule
+            "$lte": time_delta,  # Before: (Last log + Timeout) = delta
+            "$get": last_log_time})  # After last log we have
+        for log in results:
+            log_list.append(log)
+        return log_list
+
+
 def ReviewLocalLog(client,log_document,log_found):
-    pass
+    curr_step = log_document['step']
+
+
 def ReviewGlobalLog(log_document):
     pass
 
