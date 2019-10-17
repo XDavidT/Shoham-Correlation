fbba29077d1a7cdd0b942e8523f72e79b7243788
David <xdavidt@gmail.com>
2019-10-17 10:24:20 +0300

    Add insert time when offense been closed

--- ActiveFunctions/EventDone.py
+++ ActiveFunctions/EventDone.py
@@ -1,9 +1,10 @@
-from CacheHandler import load_events
 from ActionsHandlers.EmailHandler import AlertOnEmail
+import datetime
 
 def SuccessEvent(client, log_document,b_setting):
     semi_collection = client[b_setting['policy-db-name']][b_setting['semi-alert-collection-name']]
     success_collection = client[b_setting['policy-db-name']][b_setting['success-alert-collection-name']]
+    log_document['offense-close-time'] = datetime.datetime.now()  # Discover time
     success_collection.insert_one(log_document)
     semi_collection.delete_one(log_document)
     # if(log_document['alert']['email']):
@@ -13,5 +14,6 @@ def SuccessEvent(client, log_document,b_setting):
 def FailEvent(client,log_document,b_setting):
     semi_collection = client[b_setting['policy-db-name']][b_setting['semi-alert-collection-name']]
     fail_collection = client[b_setting['policy-db-name']][b_setting['fail-alert-collection-name']]
+    log_document['offense-close-time'] = datetime.datetime.now()
     fail_collection.insert_one(log_document)
     semi_collection.delete_one(log_document)
